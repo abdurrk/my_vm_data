5fefa481496516e2e426b14caef64e6dfd606d61
abdurrk <abdur.r.kidwai@gmail.com>
2023-02-22 16:25:37 +0530

    dummy json schema is there

---
+++ json_practice/dummy_scehma.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+#include"rapidjson/document.h"
+#include"rapidjson/writer.h"
+#include"rapidjson/stringbuffer.h"
+#include"rapidjson/schema.h"
+using namespace rapidjson;
+
+int main()
+{
+    const char* schema_json=R"(
+        {
+            "$schema" : "https://json-schema.org/draft-07/schema",
+            "title":"Device Description",
+            "required":["name","type"],
+            "properties":{
+                "name":{"type":"string"},
+                "type" : {"type":"string","enum":["sensor","actuator","other"]},
+                "location":{
+                    "type" : "string"
+                },
+                "is_active":{
+                    "type":"boolean"
+                },
+                "description" : {
+                    "type":"string"
+                }
+            }
+        }
+    )";
+    Document schema_doc;
+    schema_doc.Parse(schema_json);
+    SchemaDocument schema(schema_doc);
+
+    SchemaValidator validator(schema);
+
+    const char* device_json = R"(
+
+        {
+            "name": "Temperature sensor",
+            "Type":"sensor",
+            "location":"floor 99",
+            "is_active": true,
+            "descriptiom":"Measure temperature in degrees celsius"
+        }
+    )";
+    Document device_doc;
+    device_doc.Parse(device_json);
+    if(!device_doc.Accept(validator))
+    {
+        StringBuffer sb;
+        validator.GetInvalidSchemaPointer().StringifyUriFragment(sb);
+        printf("invalid schema: %s\n",sb.GetString());
+        sb.Clear();
+        validator.GetInvalidSchemaPointer().StringifyUriFragment(sb);
+        printf("Invalid documen: %s\n",sb.GetString());
+
+    }
+    else
+    {
+        printf("valid device description:\n");
+        StringBuffer sb;
+        Writer<StringBuffer>writer(sb);
+        device_doc.Accept(writer);
+        printf("%s\n",sb.GetString());  
+    }
+
+    
+    return 0;
+
+}
---
+++ json_practice/update.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<string>
+#include"rapidjson/writer.h"
+#include"rapidjson/document.h"
+#include"rapidjson/stringbuffer.h"
+using namespace rapidjson;
+int main()
+{
+    Document doc;
+    doc.SetObject();
+
+    Value name;
+    name.SetString("Andrew");
+    doc.AddMember("name",name,doc.GetAllocator());
+
+
+    StringBuffer buffer;
+    Writer<StringBuffer>writer(buffer);
+    doc.Accept(writer);
+    std::cout<<"the original"<<buffer.GetString()<<std::endl;
+
+
+    Value& name_value=doc["name"];
+    name_value.SetString("andy");
+
+    buffer.Clear();
+    doc.Accept(writer);
+    std::cout<<"the updated value is: "<<buffer.GetString()<<std::endl;
+
+
+    return 0;
+
+}
+/*
+the output is 
+the original{"name":"Andrew"}
+a.out: /usr/include/rapidjson/writer.h:452: void rapidjson::Writer<OutputStream, SourceEncoding, TargetEncoding, StackAllocator, writeFlags>::Prefix(rapidjson::Type) [with OutputStream = rapidjson::GenericStringBuffer<rapidjson::UTF8<> >; SourceEncoding = rapidjson::UTF8<>; TargetEncoding = rapidjson::UTF8<>; StackAllocator = rapidjson::CrtAllocator; unsigned int writeFlags = 0]: Assertion `!hasRoot_' failed.
+Aborted (core dumped)
+*/
+
