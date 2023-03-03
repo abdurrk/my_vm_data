9beb8f77cbc7bced15c9a20bcbbd06bcf187e376
abdurrk <abdur.r.kidwai@gmail.com>
2023-03-03 15:02:27 +0530

    json demo schema added

---
+++ json_schema/json_handler.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+#include<memory>
+#include<cstdio>
+#include"rapidjson/prettywriter.h"
+#include"rapidjson/writer.h"
+#include"rapidjson/document.h"
+#include"rapidjson/stringbuffer.h"
+#include"rapidjson/filereadstream.h"
+#define MAX 65536
+using namespace rapidjson;
+
+void Json_Validator()
+{
+    FILE* fp=fopen("skeleton.json","r");
+    if(!fp)
+    {
+        std::cout<<"error:Unable to open file"<<std::endl;
+        return;
+    }
+    char buffer[MAX];
+    FileReadStream is(fp,buffer,sizeof(buffer));
+    Document doucment;
+    doucment.ParseStream(is);
+    if(doucment.HasParseError())
+    {
+        std::cerr<<"error: failed to parse json documnet of response"<<std::endl;
+        fclose(fp);
+        return ;
+    }
+    fclose(fp);
+
+     CreateTree(doucment);
+
+
+
+}
+void CreateTree(document& json )
+{
+    std::shared_ptr<ElementManager>  ObjectElement_maneger=std::make_shared<ELementManager>();  //create shred ptr obj of elemnt manager
+    //create root element and set it to elemnt mangr
+   // for(auto element:json.GetObject())
+   for(int i=0;i<=json.Size();i++)
+    {
+        std::string str=json[i].GetString();
+        if(str == "identifier")
+        {
+
+        }
+        //get the identifier from the element json
+        //serach for parent object in the tree
+        //create structure elemnt with the help of elemnt maneger
+        //root elemnt have func get_elem_by_eddr()
+        //parent uid ==1
+        //
+
+    }
+
+}
